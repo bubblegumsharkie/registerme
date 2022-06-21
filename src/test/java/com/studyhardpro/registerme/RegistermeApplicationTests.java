a5ca4b176546658b63da5fc44b437e762f28c581
Alexey Kozlov <kozlov.alex.dev@gmail.com>
2022-06-21 10:28:18 +0300

    initial commit

---
+++ src/test/java/com/studyhardpro/registerme/RegistermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhardpro.registerme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
